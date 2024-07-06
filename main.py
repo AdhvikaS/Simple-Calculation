a7a45b7fa6d48d2c7eb3ab2efa8177a8b53506a3
AdhvikaS <adhvikasundaramurali@gmail.com>
2024-07-06 20:14:40 +0530

    Simple Calculation

---
+++ main.py
@@ -0,0 +1,13 @@
+#Adhvika
+a=int(input("Enter Intger "))
+b=int(input("Enter Intger "))
+c=str(input("Enter Operation "))
+if c=='+':
+    print(a+b)
+if c=='-':
+    print(a-b)
+if c=='*':
+    print(a*b)
+if c=='/':
+    print(a/b)
+
